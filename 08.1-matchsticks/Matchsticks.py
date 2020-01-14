171109269e9091a91e4a67cc9b797801de95f2e5
Kehvarl <kehvarl@gmail.com>
2020-01-13 20:43:34 -0700

    Once more, reading the actual instructions was important

---
+++ 08.1-matchsticks/Matchsticks.py
@@ -0,0 +1,31 @@
+
+def get_length(input_line):
+    length = 0
+    index = 0
+    while index < len(input_line):
+        if index == len(input_line)-1:
+            length += 1
+            index += 1
+        elif input_line[index] != "\\":
+            length += 1
+            index += 1
+        elif input_line[index + 1] in [str('\\'), "\""]:
+            length += 1
+            index += 2
+        elif input_line[index + 1] == "x":
+            length += 1
+            index += 4
+    return length
+
+
+input_file = open("input.txt", "r").read().split("\n")
+
+total_length = 0
+for line in input_file:
+    test = line.strip().strip("\"")
+    tlen = get_length(test)
+    print(line)
+    print(test, tlen)
+    total_length += len(line) - tlen
+
+print(total_length)
