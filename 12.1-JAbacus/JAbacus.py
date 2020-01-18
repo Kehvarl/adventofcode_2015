695520f000355d9f337853367d700ea4cc160103
Adam Corbett <kehvarl@gmail.com>
2020-01-17 20:00:20 -0700

    pep8 cleanup

--- 12.1-JAbacus/JAbacus.py
+++ 12.1-JAbacus/JAbacus.py
@@ -1,22 +1,24 @@
 import json
 
 
-def sum_nums(input_node):
+def sum_nums(input_node, ignore_list=None):
+    if ignore_list is None:
+        ignore_list = []
     sum_total = 0
 
     if isinstance(input_node, dict):
         for k, v in input_node.items():
-            if k == "red" or v == "red":
+            if k in ignore_list or v in ignore_list:
                 return 0
             if isinstance(v, dict) or isinstance(v, list):
-                sum_total += sum_nums(v)
+                sum_total += sum_nums(v, ignore_list)
             elif isinstance(v, int):
                 sum_total += v
 
     elif isinstance(input_node, list):
         for item in input_node:
             if isinstance(item, dict) or isinstance(item, list):
-                sum_total += sum_nums(item)
+                sum_total += sum_nums(item, ignore_list)
             elif isinstance(item, int):
                 sum_total += item
 
@@ -31,8 +33,8 @@ input_file = open("input.txt", "r").read().split("\n")
 total = 0
 for line in input_file:
     data = json.loads(line)
-    total += sum_nums(data)
-    print(sum_nums(data))
-
+    total += sum_nums(data, ["red"])
 
 print(total)
+
+
