1b194005559945c3e3459fd32ebacda6171e9045
Adam Corbett <kehvarl@gmail.com>
2020-01-18 21:37:26 -0700

    Solutions to part 1 and 2
    Get all permutations
    
    Get happiness for all permutations
    
    Find greatest happiness.

---
+++ 13.1-KODT/kodt.py
@@ -0,0 +1,60 @@
+from pprint import pprint
+
+
+def parseline(input_line):
+    tokens = input_line.strip(".").split(" ")
+
+    parse_subject = tokens[0]
+    parse_amount = int(tokens[3])
+    parse_amount = -parse_amount if tokens[2] == "lose" else parse_amount
+    parse_target = tokens[-1]
+
+    return parse_subject, parse_amount, parse_target
+
+
+def get_permutations(lst):
+    if len(lst) == 0:
+        return []
+    if len(lst) == 1:
+        return [lst]
+    l = []
+    for i in range(len(lst)):
+        m = lst[i]
+        rest = lst[:i] + lst[i + 1:]
+
+        for p in get_permutations(rest):
+            l.append([m] + p)
+    return l
+
+
+graph = {}
+
+input_text = open("input.txt", "r").read().split("\n")
+
+
+for line in input_text:
+    subject, amount, target = parseline(line)
+    if not graph.get(subject):
+        graph[subject] = {}
+    graph[subject][target] = amount
+
+
+subjects = []
+for key in graph:
+    subjects.append(key)
+
+seating_arrangements = get_permutations(subjects)
+totals = []
+
+for arrangement in seating_arrangements:
+    total = 0
+    for index in range(len(arrangement) - 1):
+        total += graph[arrangement[index]][arrangement[index + 1]]
+        total += graph[arrangement[index + 1]][arrangement[index ]]
+    total += graph[arrangement[-1]][arrangement[0]]
+    total += graph[arrangement[0]][arrangement[-1]]
+    print(arrangement, total)
+    totals.append(total)
+
+print(max(totals))
+
---
+++ 13.2-KODT/kodt.py
@@ -0,0 +1,71 @@
+from pprint import pprint
+
+
+def parseline(input_line):
+    tokens = input_line.strip(".").split(" ")
+
+    parse_subject = tokens[0]
+    parse_amount = int(tokens[3])
+    parse_amount = -parse_amount if tokens[2] == "lose" else parse_amount
+    parse_target = tokens[-1]
+
+    return parse_subject, parse_amount, parse_target
+
+
+def get_permutations(lst):
+    if len(lst) == 0:
+        return []
+    if len(lst) == 1:
+        return [lst]
+    l = []
+    for i in range(len(lst)):
+        m = lst[i]
+        rest = lst[:i] + lst[i + 1:]
+
+        for p in get_permutations(rest):
+            l.append([m] + p)
+    return l
+
+
+def get_happiness(people, a, b):
+    happiness = 0
+    if a == "Me" or b == "Me":
+        return  0
+    happiness += people[a][b]
+    happiness += people[b][a]
+    return happiness
+
+
+
+graph = {}
+
+input_text = open("input.txt", "r").read().split("\n")
+
+
+for line in input_text:
+    subject, amount, target = parseline(line)
+    if not graph.get(subject):
+        graph[subject] = {}
+    graph[subject][target] = amount
+
+
+subjects = []
+for key in graph:
+    subjects.append(key)
+
+subjects.append("Me")
+
+seating_arrangements = get_permutations(subjects)
+totals = []
+
+for arrangement in seating_arrangements:
+    total = 0
+    for index in range(len(arrangement) - 1):
+        total += get_happiness(graph, arrangement[index], arrangement[index + 1])
+    total += get_happiness(graph, arrangement[-1], arrangement[0])
+
+    print(arrangement, total)
+    totals.append(total)
+
+print(max(totals))
+
